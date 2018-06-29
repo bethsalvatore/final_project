27b7b339696a7e88313d4f54dcdd5a44ed333979
Beth Salvatore <bethsalvatore2018@gmail.com>
2018-06-28 21:16:35 -0400

    first commit

---
+++ resources/views/generalSearch.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+//link to bootstrap
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"
+integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+
+	<title>General Search</title>
+</head>
+<body>
+  <h1>This is the general search page.</h1>
+
+<script src="main.js"></script>
+</body>
+</html>
---
+++ resources/views/registrationForm.blade.php
@@ -0,0 +1,14 @@
+
+
+<?php
+
+  function registrationForm(){
+    return '<form class="form-inline mt-5" method="get" action="">
+    <input type="text" class="rounded-0 form-control mb-2 mr-sm-2 mb-sm-0"
+    id="firstName" name="firstName" placeholder="">
+    <input type="text" class="rounded-0form-control" id="lastName"
+    name="lastName" placeholder="" maxlength="25" size="6"
+	  </div> <button type="submit" class="btn btn-secondary rounded-0">Add Color</button>
+    </form></div>';
+}
+?>
